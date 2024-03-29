4d4764e9f24c3e0c1fdb96daf58e2e2131ce43b3
Theo GILABERT <theogilat@gmail.com>
2024-03-29 08:52:52 +0100

    Ajoutez des fichiers projet.

---
+++ UtilitairesSLAM/Classes/Pile.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MesOutils
+{
+    /// <summary>
+    /// Pile contenant pouvant contenir des entiers.
+    /// On ajoute après le dernier élément ajouté
+    /// On retire toujours le dernier élément ajouté
+    /// </summary>
+    class Pile<T>
+    {
+        /// <summary>
+        /// Nombre maximum d'élements de la Pile
+        /// </summary>
+        //private int nbMaxElt;
+
+        /// <summary>
+        /// Liste contenant les éléments de la pile
+        /// </summary>
+        private List<T> elements;
+
+        public Pile()
+        {
+            //this.nbMaxElt = nbMaxElt;
+            this.elements = new List<T>();
+        }
+
+
+        //public bool PilePleine()
+        //{
+        //    return (this.nbMaxElt == this.elements.Count);
+        //}
+
+        public bool PileVide()
+        {
+            return this.elements.Count == 0;
+        }
+
+        public void Empiler(T valeur)
+        {
+            //if (!PilePleine())
+            //{
+            this.elements.Add(valeur);
+            //}
+            //else
+            //{
+            //    throw new Exception("Pile pleine, impossible d'empiler un élément");
+            //}
+        }
+
+        public T Depiler()
+        {
+            if (PileVide())
+            {
+                throw new Exception("Impossible de Dépiler , la pile est déjà vide");
+            }
+            else
+            {
+
+
+                T valeurDepilee = this.elements[this.elements.Count - 1];
+
+                this.elements.RemoveAt(this.elements.Count - 1);
+
+                return valeurDepilee;
+            }
+
+
+
+        }
+
+        /// <summary>
+        /// Obtient le nombre d'éléments actuellement présents dans la Pile
+        /// </summary>
+        public int Count { get => this.elements.Count; }
+
+    }
+}
---
+++ UtilitairesSLAM/Classes/Utilitaire.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Utilitaires
+{
+    public abstract class Utilitaire
+    {
+        public static int SaisirNb()
+        {
+
+            int temp = 0;
+            bool isNumber = false;
+            do
+            {
+                Console.WriteLine("Saisisez un nombre entier :");
+                try
+                {
+                    temp = Convert.ToInt32(Console.ReadLine());
+                    isNumber = true;
+
+
+                }
+                catch (FormatException) { }
+
+            } while (!isNumber);
+            return temp;
+        }
+
+        public static int SaisirNb(int pMin)
+        {
+            int nb = 0;
+            do
+            {
+
+                try
+                {
+                    Console.WriteLine("Veuillez saisir un nombre entier supérieur à " + pMin + " : ");
+                    nb = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException) { }
+
+
+            }
+            while (nb < pMin);
+            return nb;
+        }
+
+
+
+        public static int SaisirNb(int pMin, int pMax)
+        {
+            int nb = 0;
+            do
+            {
+
+                try
+                {
+                    Console.WriteLine("Veuillez saisir un nombre entier entre " + pMin + " et " + pMax + " : ");
+                    nb = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException) { }
+
+
+            }
+            while (nb < pMin || nb > pMax);
+            return nb;
+        }
+    }
+}
